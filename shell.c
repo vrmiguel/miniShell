e6f441d3d164d85818d0f0c52f6cfa434a32428e
vrmiguel <36349314+vrmiguel@users.noreply.github.com>
2019-03-13 17:12:34 -0300

    Add files via upload
    BUGGY!

--- shell.c
+++ shell.c
@@ -9,30 +9,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
 
 #define true 1
-#define BUFSIZE 1024
 
-//Protótipos
+    //Protótipos - Function signatures
 char *getInput();
 char **parser(char *input);
-void type_prompt();
+void typePrompt();
+void printWelcome();
+int run(char ** parsed);
+int strcmpr(char * str1, char *str2);
+
+
+    //Variáveis globais
+//char* acceptableCommands[] = {"ifconfig", "ls", "quit"};
+char* acceptableCommands[] = {"ls", "quit"};
+int acceptableCommandsNo = 2;
+int parsedItemsNo; // quantidade de palavras tokenizadas na string atual
 
 int main(int argv, char **argc)
 {
     //char* input;
     //char** parsed;
-
+    printWelcome();
+    char cwd[BUFSIZ]; /* Inicializa string para pasta atual, respeitando o limite máximo de caracteres que o stdin pode transferir */
     while (true)
     {
-        type_prompt();
+        typePrompt(); // Exibe "~$ "
+        int ret; // Variável para teste de retorno de execução | execution r
         char *input = getInput(); // Adquira input
-        char cwd[BUFSIZE];        // Inicializa string para pasta atual
         getcwd(cwd, sizeof(cwd)); //Grava o nome da pasta atual
-        printf("%s\n", input);
         char **parsed = parser(input);
-
-        free(input); //Para ser executada por último
+        ret = run(parsed);
+        free(input); //Libera memória alocada para input e parsed
         free(parsed);
     }
     return 0;
@@ -40,43 +51,82 @@ int main(int argv, char **argc)
 
 char *getInput()
 {
-    char *input = (char *)malloc(sizeof(char)); //Aloca espaço para primeira letra
-    char c = getchar();
-    input[0] = c;
+    /* Lê e retorna caracteres do stdin. Por motivos de economia de memória, o programa salva a
+    string do usuário de maneira dinâmicamente crescente. */
+    char *input = (char *) malloc(sizeof(char)); //Aloca espaço para primeira letra
+    char c = getchar(); // Recebe primeira letra..
+    input[0] = c;       //..e inicializa input com ela.
+
     int i = 1;
-    while (((c = getchar()) != '\n') && (c != EOF) && (i < BUFSIZE - 2))
+
+    while (((c = getchar()) != '\n') && (c != EOF) && (i < BUFSIZ - 2))
     {
-        input = realloc(input, (i+1)*sizeof(char)); // Aloca espaço para mais uma letra.
-        input[i] = c;                         //Insere letra no vetor
+        input = realloc(input, (i+1)*sizeof(char)); // Aloca espaço para mais uma letra
+        input[i] = c;    //Insere letra no vetor
         i++;
     }
-    input[i] = '\0';                          //Insere terminador de string.
-    printf("From getInput: \"%s\"\n", input); // exibição de teste
+    input[i] = '\0';    //Insere terminador de string.
     return input;
 }
 
+/* Divide a string dada pela variável input  */
 char **parser(char *input)
 {
     char **parsed = malloc(sizeof(char *)); //Aloca espaço para primeira palavra
-    char *token = strtok(input, " ");
+    char *token = strtok(input, " "); // Cria primeiro token e mantém o ponteiro input em estado interno
     int i = 0;
-    parsed[0] = token;
-    printf("From Parser: %s\n", parsed[0]);
+    parsed[0] = token; // Inicializa parsed com o primeiro token
+
+    // printf("Do Parser: %s\n", parsed[0]); // teste
+
     for (;;)
     {
-        i++;
-        token = strtok(NULL, " ");
-        if (token == NULL)
+        token = strtok(NULL, " "); //Continua criando tokens do ponteiro em estado interno
+        if (token == NULL) //Caso encontrada o fim da string original, feche o loop
             break;
-        parsed = realloc(parsed, (i + 1) * sizeof(char *));
+        i++;
+        parsed = realloc(parsed, (i + 1) * sizeof(char *)); // Aloca espaço para mais uma palavra
         parsed[i] = token;
-        //*(parsed + i) = token;
-        printf("From Parser: %s\n", parsed[i]); // print de teste
+        //printf("Do Parser: %s\n", parsed[i]); // print de teste
     }
+    printf("ParsedListSize = %d\n", i);
+    parsedItemsNo = i+1; // Registra quantas palavras foram tokenizadas
     return parsed;
 }
 
-void type_prompt()
+int run(char ** parsed) // EM TESTE, NÃO FUNCIONA
+{
+    pid_t testPID; // valor para teste pai/filho
+    int i, aux, foundIndex = -1;
+    if(parsedItemsNo<=acceptableCommandsNo)
+      aux = acceptableCommandsNo;
+    else
+      aux = parsedItemsNo;
+
+    for(i=0; i<aux-1; i++)
+    {
+        if( !strcmpr(parsed[i], acceptableCommands[i]) )
+            printf("Deu certo com %d\n", i);
+    }
+
+    //execvp(parsed[0], parsed);
+    //printf("%s", acceptableCommands[2]);
+    return 1;
+}
+
+void typePrompt()
 {
     printf("~$ ");
+}
+
+void printWelcome()
+{
+    printf("\n   -------- miniShell --------\n\nVinícius R. Miguel, Lucas S. Vaz & Gustavo B. de Oliveira\ngithub.com/vrmiguel/miniShell -- Unifesp -- Março de 2019\n\n");
+}
+
+int strcmpr(char* s1, char* s2)
+{
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(unsigned char*)s1-*(unsigned char*)s2;
 }
